6bd514e88f5a00df126a2758344d9a93b33c2bcd
ouryuu <ouryuu@users.noreply.github.com>
2021-03-02 22:40:27 +0800

    20210302

--- ch01/img_show.py
+++ ch01/img_show.py
@@ -1,8 +1,12 @@
 # coding: utf-8
 import matplotlib.pyplot as plt
 from matplotlib.image import imread
+import os
 
-img = imread('../dataset/lena.png') # 画像の読み込み
+print(os.getcwd())
+print(__file__)
+#img = imread('../dataset/lena.png') # 画像の読み込み
+img = imread(os.getcwd()+r'\dataset\lena.png')
 plt.imshow(img)
 
 plt.show()
